949cbdd82101b0ed461a06837dabd9fdd094d60e
AnishGupta25 <anishgupta2522@gmail.com>
2021-06-07 22:40:19 +0530

    dom - html & css

---
+++ module_2/Todo_app/activity/script.js
@@ -0,0 +1,47 @@
+let addtodolist = document.querySelector(".add-todo");
+let todoinput = document.querySelector(".todo-input");
+let todolist = document.querySelector(".todo-list-container");
+
+todoinput.addEventListener( "keypress" , function(e){
+    if(e.key == "Enter"){
+        addtodo();
+    }
+});
+
+addtodolist.addEventListener( "click" , function(){
+    addtodo();
+});
+
+
+function addtodo(){
+    let todoinputvalue = todoinput.value;
+    if(todoinputvalue){
+        console.log(todoinputvalue);
+        appendtodo(todoinputvalue);
+        todoinput.value = "";
+    }
+}
+
+function appendtodo(todo){
+    let todolistdiv = document.createElement("div");
+    todolistdiv.classList.add("todo-items");
+
+    let ptag = document.createElement("p");
+    ptag.classList.add("todo-input");
+    ptag.textContent = todo;
+
+    let deletebutton = document.createElement("button");
+    deletebutton.classList.add(".delete-todo");
+    deletebutton.textContent = "Delete";
+
+    deletebutton.addEventListener( "click" , deletetodo);
+
+    todolistdiv.append(ptag);
+    todolistdiv.append(deletebutton);
+
+    todolist.append(todolistdiv);
+}
+
+function deletetodo(e){
+    e.target.parentNode.remove();
+}
