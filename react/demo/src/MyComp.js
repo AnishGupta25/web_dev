a3f643cb9e88b37f311db24fe6e79bc8a73f0fb0
AnishGupta25 <anishgupta2522@gmail.com>
2021-08-03 19:56:01 +0530

    multi compnents

--- react/demo/src/MyComp.js
+++ react/demo/src/MyComp.js
@@ -3,13 +3,16 @@ import React from "react";
 class MyComp extends React.Component{
     
     state = {
-        somenumber:25
+        somenumber : 0
     }
 
     render = () => {
         return(
         <div>
             <h1>{ this.state.somenumber}</h1>
+            <button onClick = { () =>{
+                this.setState({somenumber :  this.state.somenumber + 1})
+            }}> this is button</button>
         </div>
         );
     };
