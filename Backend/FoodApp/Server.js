19767de7cb7312707b1a3fd11df4a287bdfde394
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-13 21:46:56 +0530

    get , post , patch , delete using postman

--- Backend/FoodApp/Server.js
+++ Backend/FoodApp/Server.js
@@ -5,13 +5,34 @@ let port = '8080'
 app.listen(port , function(){
     console.log('Server is listening on port 8080');
 });
+app.use(express.json());
+let user ={};
 
 app.get('/' , (req, res) => {
     // console.log('<h1>Null</h1>');
-    res.send('<h>Hello</h>');
+    res.send('Home page');
 })
 
 app.get('/home' , (req, res) => {
     // console.log('<h1>Home</h1>');
-    res.send('<h>Home</h>');
+    res.json(user);
+})
+
+app.post('/home' , (req,res) => {
+    user = req.body;
+    console.log(user);
+    res.send("data has been added and updated")
+})
+
+app.patch('/home',(req , res) => {
+    let obj = req.body;
+    for(let key in obj){
+        user[key] = obj[key];
+    }
+    res.json(user);
+});
+
+app.delete('/home',(req , res) => {
+    user = {"hello" : "hell0"};
+    res.json(user);
 })
