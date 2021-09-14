98322625f0cce2663ebc5706afee6f57f5793445
AnishGupta25 <anishgupta2522@gmail.com>
2021-09-14 21:36:43 +0530

    axios , route

--- Backend/FoodApp/Server.js
+++ Backend/FoodApp/Server.js
@@ -1,43 +1,92 @@
-const express = require('express');
-const app = express();
+const express=require('express');
 
-let port = '8080'
-app.listen(port , function(){
-    console.log('Server is listening on port 8080');
+const app=express();
+// const router=express.Router();
+app.listen('5000',function(){
+    console.log('server listening on port 5000');
 });
+
 app.use(express.json());
-let user ={};
+app.use(express.static('public'));
+const userRouter=express.Router();
+const authRouter=express.Router();
+app.use('/user',userRouter);
+app.use('/auth',authRouter);
+//mounting in express
+userRouter
+.route('/')
+.get(getUser)
+.post(createUser)
+.patch(updateUser)
+.delete(deleteUser);
 
-app.get('/' , (req, res) => {
-    // console.log('<h1>Null</h1>');
-    res.send('Home page');
-})
+userRouter
+.route('/:id')
+.get(getUserById);
 
-app.get('/home' , (req, res) => {
-    // console.log('<h1>Home</h1>');
+authRouter
+.route('/signup')
+.post(signupUser);
+
+function signupUser(req,res){
+    // let userDetails=req.body;
+    // let name=userDetails.name;
+    // let email=userDetails.email;
+    // let password=userDetails.password;
+
+    let{email,name,password}=req.body;
+    user.push({email,name,password});
+    console.log('user',req.body);
+    res.json({
+        message:'user signedUp',
+        user:req.body
+    });
+}
+
+
+let user=[];
+// client <- server
+//crud- create read update delete
+//read
+app.get('/',(req,res)=>{
+    res.send('Home Page');
+});
+
+// app.get('/user',getUser);
+
+function getUser(req,res){
     res.json(user);
-})
+}
 
-app.post('/home' , (req,res) => {
-    user = req.body;
-    console.log(user);
-    res.send("data has been added and updated")
-})
-
-app.patch('/home',(req , res) => {
-    let obj = req.body;
+//post request
+// client-> server 
+//create
+// app.post('/user',createUser);
+function createUser(req,res){
+    user=req.body;
+    // console.log(req.body);
+    res.send('data has been added succesfully');
+}
+//update
+// app.patch('/user',updateUser);
+function updateUser (req,res){
+    let obj=req.body;
     for(let key in obj){
-        user[key] = obj[key];
+        user[key]=obj[key];
     }
     res.json(user);
-});
-
-app.delete('/home',(req , res) => {
-    user = {"hello" : "hell0"};
+};
+//delete 
+// app.delete('/user',deleteUser);
+function deleteUser(req,res){
+    user={};
     res.json(user);
-})
+    // res.send('ussr has been deleted');
+}
+//param route
+// app.get('/user/:id',getUserById);
 
-app.get('/home/:id' , (req, res) => {
-    // console.log('<h1>Home</h1>');
-    res.json(req.params);
-})
+function getUserById(req,res){
+    console.log(req.params);
+    res.json(req.params.id);
+}
